f2468fdb624e2366f97b2836b9511eec26d2d961
chaquen <edgar.guzman21@gmail.com>
2019-01-29 10:22:42 -0500

    se corrigen error en cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {    //EJECUTO EL COMANDO DIARIAMENTE A MEDIANOCHE   
          $schedule->command('ValidarTransaccion:ValidarTransaccion')
-                  ->hourly();
+                  ->daily();
     }
 
     /**
